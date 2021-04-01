c2819bc8fa7d971f9422c922df01a5ff9191bdbd
Kamaludien Sonday <kamalsonday.158@gmail.com>
2021-04-01 11:08:08 +0200

    218168128

---
+++ src/main/java/za/ac/cput/assignment_1/BankAccount.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.assignment_1;
+
+/**
+ *
+ * @author Kamaludien
+ */
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private double balance;
+
+    public BankAccount(String firstName, String lastName, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" + "firstName=" + firstName + ", lastName=" + lastName + ", balance=" + balance + '}';
+    }
+}
